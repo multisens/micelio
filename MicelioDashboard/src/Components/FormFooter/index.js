504467cf294efe5d03679273ff2121a8676d6f95
lucassargeir <sargeirolucas@gmail.com>
2021-04-07 20:58:48 -0300

    Export Insomnia

--- MicelioDashboard/src/Components/FormFooter/index.js
+++ MicelioDashboard/src/Components/FormFooter/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './style.css';
+import {Link} from 'react-router-dom';
 
 const FormFooter = (props) =>{
 
     return (
     	<span>{props?.beginingText}
-            <a href={ (props?.url)? props.url:"#"}>{props?.linkText}</a>
+            <Link to={ (props?.url)? props.url:"#"}>{props?.linkText}</Link>
             {props?.endingText}
         </span>
     );
