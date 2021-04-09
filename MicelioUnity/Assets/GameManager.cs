9cec85e9feef30df5bbd30a26a40196e45ac1cea
igor feital <igfeitall@gmail.com>
2021-04-09 16:14:15 -0300

    chama

---
+++ MicelioUnity/Assets/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+
+	public string URL = "https://albali.eic.cefet-rj.br/micelio/api";
+	public string port = "80";
+	public string token = "toki";
+	public string rota = "/user";
+	public string method = "POST";
+	public string payload = "{\"username\": \"Lucas\",\"password\":\"minha-senha\"}";
+
+    void Start()
+    {
+     
+        Micelio micelio = new Micelio(URL,port,token);
+        micelio.SendAPIRequest(rota,method,payload);
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
