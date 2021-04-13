8bd801d3fecec72a5a24837d69d65e71e32d0015
Marcelo Barros <mhbarros99@gmail.com>
2021-04-12 21:10:11 -0300

    Cards

--- MicelioDashboard/src/pages/Home/index.js
+++ MicelioDashboard/src/pages/Home/index.js
@@ -16,14 +16,35 @@ function Home() {
 
         <MainMenu selected={'start'} />
 
-        <main class={'gamelist'}>
-          <h2>Lista de salas</h2>
-          <ul>
-            <li>Jogo 1</li>
-            <li>Jogo 2</li>
-            <li>Jogo 3</li>
-            <li>Jogo 4</li>
-          </ul>
+        <main className={'gamelist-container'}>
+
+          <div className={'gamecards'}>
+            <ul>
+              <li>
+                Card 1
+              </li>
+              <li>
+                Card 2
+              </li>
+              <li>
+                Card 3
+              </li>
+              <li>
+                Card 4
+              </li>
+            </ul>
+          </div>
+
+          <div className={'gamelist'}>
+            <h2>Lista de salas</h2>
+            <ul>
+              <li>Jogo 1</li>
+              <li>Jogo 2</li>
+              <li>Jogo 3</li>
+              <li>Jogo 4</li>
+            </ul>
+          </div>
+
         </main>
 
       </div>
