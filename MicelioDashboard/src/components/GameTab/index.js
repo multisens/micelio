560b19c4581ddaa12779e5a615671f0d92f9ec8a
lucassargeir <sargeirolucas@gmail.com>
2022-01-06 22:58:38 -0300

    aba de grupo de sessões e input de seleção

--- MicelioDashboard/src/components/GameTab/index.js
+++ MicelioDashboard/src/components/GameTab/index.js
@@ -1,4 +1,4 @@
-import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react"
+import { Tabs, TabList, TabPanels, Tab, TabPanel, Select } from "@chakra-ui/react"
 
 import SessionGroupList from "../../components/SessionGroupList"
 import Visualization from "../Visualization"
@@ -9,15 +9,33 @@ const GameTab = ({ groupList, gameToken }) => {
       <TabList>
         <Tab>Grupos criados</Tab>
         <Tab>Visualizações</Tab>
+        <Tab>Grupos de Visualização</Tab>
       </TabList>
-
       <TabPanels>
         <TabPanel>
           <SessionGroupList groups={groupList} />
         </TabPanel>
+
         <TabPanel>
-          <Visualization />
+          <Select maxWidth={450}>
+            <option value='option0' disabled={true}>Escolha a sessão que deseja ver</option>
+            <option value='option1'>Player: mhbarros  - 10/06/2021</option>
+            <option value='option2'>Player: igdark    - 10/06/2021</option>
+          </Select>
+          <br/>
+          <Visualization component_id="single"/>
         </TabPanel>
+
+        <TabPanel>
+          <Select maxWidth={300}>
+            <option value='option0' disabled={true}>Escolha o grupo de sessões</option>
+            <option value='option1'>45854</option>
+            <option value='option2'>46229</option>
+          </Select>
+          <br/>
+          <Visualization component_id="group"/>
+        </TabPanel>
+
       </TabPanels>
     </Tabs>
   )
