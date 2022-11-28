4621c6eed00a455133bd06675b24a1c386c087e6
Marcelo Barros <mhbarros99@gmail.com>
2022-11-28 10:44:08 -0300

    Instalação vega-lite

---
+++ MicelioDashboardNext/src/components/Visualization/index.js
@@ -0,0 +1,10 @@
+import * as vl from 'vega-lite-api';
+import { useEffect } from 'react';
+
+const Visualization = () => {
+  useEffect(() => {}, []);
+
+  return <h1>Hello world</h1>;
+};
+
+export default Visualization;
---
+++ MicelioDashboardNext/src/pages/profile/index.js
@@ -0,0 +1,11 @@
+import Header from '../../components/Header';
+
+const Profile = () => {
+  return (
+    <>
+      <Header pageName={'profile'} />
+    </>
+  );
+};
+
+export default Profile;
